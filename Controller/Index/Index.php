5307906a88b7b768fc271ad45147346400319ca5
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-01-02 23:52:47 +0000

    Register module Ivan_Amasty - add page with form

---
+++ Controller/Index/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ivan\Amasty\Controller\Index;
+
+use \Magento\Framework\App\ActionInterface;
+use \Magento\Framework\Controller\ResultFactory;
+
+class Index implements ActionInterface
+{
+	private $resultFactory;
+
+	public function __construct(
+        ResultFactory $resultFactory
+    )
+	{
+		$this->resultFactory = $resultFactory;
+	}
+
+	public function execute()
+	{
+	    return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+  }
+}
